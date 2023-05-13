f93059e7e91bd88e1f1e781e0e1d17f7abb5899d
VladTomici14 <vladtomici@gmail.com>
2023-05-13 21:28:04 +0300

    Prepared last changes for deploying the site with the database

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const cors = require("cors");
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
 
-const app = express();
-
+const cors = require("cors")
 var corsOptions = {
     origin: "http://localhost:8081"
 };
 
-app.use(cors(corsOptions));
-// parse requests of content-type - application/json
-app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+const express = require("express")
+const app = express()
+const bcrypt = require("bcrypt") // Importing bcrypt package
+const passport = require("passport")
+const initializePassport = require("./passport-config")
+const flash = require("express-flash")
+const session = require("express-session")
+const methodOverride = require("method-override")
+
 
 const db = require("./app/models");
 
@@ -22,21 +26,96 @@ db.sequelize.sync()
     .catch((err) => {
         console.log("Failed to sync db: " + err.message);
     });
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
-// drop the table if it already exists
-//     db.sequelize.sync({ force: true }).then(() => {
-//         console.log("Drop and re-sync db.");
-//     });
+const users = []
 
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
-});
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: "garland",
+    resave: false, // We wont resave the session variable if nothing is changed
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride("_method"))
+
+// Configuring the register post functionality
+app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+}))
+
+// Configuring the register post functionality
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        console.log(users); // Display newly registered in the console
+        res.redirect("/login")
+
+    } catch (e) {
+        console.log(e);
+        res.redirect("/register")
+    }
+})
+
+// Routes
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render("index.ejs", {name: req.user.name})
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render("login.ejs")
+})
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render("register.ejs")
+})
+// End Routes
+
+// app.delete('/logout', (req, res) => {
+//     req.logOut()
+//     res.redirect('/login')
+//   })
+
+app.delete("/logout", (req, res) => {
+    req.logout(req.user, err => {
+        if (err) return next(err)
+        res.redirect("/")
+    })
+})
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/login")
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect("/")
+    }
+    next()
+}
 
 require("./app/routes/tutorial.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+
+app.listen(3000)
